21aa062464576f1b131adde1f549a1d289e4ddc1
linfeng <linfengchina88@gmail.com>
2012-10-13 13:48:37 +0800

    Add main.m

---
+++ EBook/Ebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EBook
+//
+//  Created by Lin Feng on 12-9-29.
+//  Copyright (c) 2012年 imohoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
